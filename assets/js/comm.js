afd456c23e97bbe179f4b0285359a2b1260b4951
han0won <han00won@gmail.com>
2024-02-23 12:36:11 +0900

    타이틀 애니메이션 효과 수정..

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,11 +1,51 @@
-// 타이틀 박스 효과
-$(document).ready(function() {
-    $('.ani-title').each(function(index) {
-      $(this).delay($(this).data('delay')).queue(function(){
-        $(this).addClass('animate-in');
-      });
-    });
+// 헤더
+document.addEventListener("DOMContentLoaded", function() {
+  const gnb = document.getElementById("gnb");
+  const depth2Bg = document.querySelector(".depth2-bg");
+
+  gnb.addEventListener("mouseover", function() {
+      depth2Bg.style.display = "block";
+  });
+
+  gnb.addEventListener("mouseout", function() {
+      depth2Bg.style.display = "none";
+  });
 });
+
+
+// $(document).ready(function() {
+//   $('.ani-title').each(function(index) {
+//     $(this).delay($(this).data('delay')).queue(function(){
+//       $(this).addClass('animate-in');
+//     });
+//   });
+// });
+
+
+// 타이틀섹션 테스트 . . . 
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var windowTop = $(this).scrollTop();
+    var titlePageTop = $('.title-page').offset().top;
+    
+    // If the top of the window is at least the top of the title-page
+    if (windowTop >= titlePageTop) {
+      $('.ani-title').each(function(index) {
+        $(this).delay($(this).data('delay')).queue(function(){
+          $(this).addClass('animate-in');
+        });
+      });
+    }
+  }).scroll(); // Trigger scroll event initially
+});
+
+
+
+// 타이틀섹션 테스트 . . . (끝)
+
+
+
   
 // 연구분야 섹션 커서 이미지
 document.addEventListener('DOMContentLoaded', function() {
